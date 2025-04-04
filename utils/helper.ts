ea1e1c3c82a4b7b7509d9b98346c41e2d94d9c57
huixu2203 <karan.kky2000@gmail.com>
2025-04-05 02:30:03 +0530

    corrected helper.ts

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,2 +1,2 @@
 export const isDev = process.env.NODE_ENV === 'development';
-export const ORIGIN_URL = isDev?'http://localhost:3000':'';
+export const ORIGIN_URL = isDev?'http://localhost:3000':'https://chota-i6fzrrxyw-drolphinos-projects.vercel.app/';
